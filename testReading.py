405882f05524306810873451f4c71ebc8113d035
scheerp <patrick-scheer@web.de>
2022-05-26 19:42:41 +0200

    asdasd

--- testReading.py
+++ testReading.py
@@ -1,26 +1,40 @@
 #!/usr/bin/env python
 
-import RPi.GPIO as GPIO
+#import RPi.GPIO as GPIO
+#from mfrc522 import SimpleMFRC522
+#import requests
+#import time
+#import board
+#import neopixel
+#
+#pixels = neopixel.NeoPixel(board.D18, 2)
+#reader = SimpleMFRC522()
+#
+#failCounter = 0
+#successCounter = "nix"
+#
+#while failCounter < 4:
+#	GPIO.cleanup()
+#	time.sleep(10)
+#	id, text = reader.read()
+#	print("start reading")
+#	pixels.fill((40,0,80))
+#	print(text)
+#	if (text.find("spotify") != 1):
+#		failCounter +=1
+#		print("failed ++", failCounter)
+	
+from time import sleep
+import sys
 from mfrc522 import SimpleMFRC522
-import requests
-import time
-import board
-import neopixel
-
-pixels = neopixel.NeoPixel(board.D18, 2)
 reader = SimpleMFRC522()
 
-failCounter = 0
-successCounter = "nix"
-
-while failCounter < 4:
+try:
+	while True:
+		print("Hold a tag near the reader")
+		id, text = reader.read()
+		print("ID: %s\nText: %s" % (id,text))
+		sleep(5)
+except KeyboardInterrupt:
 	GPIO.cleanup()
-	time.sleep(10)
-	id, text = reader.read()
-	print("start reading")
-	pixels.fill((40,0,80))
-	print(text)
-	if (text.find("spotify") != 1)
-		failCounter +=1
-		print("failed ++", failCounter)
-	
+	raise
