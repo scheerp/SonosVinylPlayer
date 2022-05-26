d80d2bd62ca7d7d3d2abd372e67a4b792a2dcb98
scheerp <patrick-scheer@web.de>
2022-05-26 20:11:10 +0200

    asdasdasd

--- VinylPlayerAutoStart.py
+++ VinylPlayerAutoStart.py
@@ -10,26 +10,36 @@ import neopixel
 pixels = neopixel.NeoPixel(board.D18, 2)
 reader = SimpleMFRC522()
 
-failCounter = 0
+emptyCount = 0
 previousRead = "nix"
 
 while True:
 	GPIO.cleanup()
+	print("EPMTYCOUNT: ", emptyCount)
 	print("Ready for some tunes!")
 	pixels.fill((40,0,80))
-	id, text = reader.read()
+	id, text = reader.read_no_block()
 
-	if (text.find("spotify") != -1 and previousRead != text):
-		getRequest = "http://192.168.2.149:5005/Foyer/spotify/now/" + text.strip()
-		r = requests.get(getRequest)
-		previousRead = text
-		pixels.fill((0, 200, 0))
-		time.sleep(0.2)
-		pixels.fill((0, 0, 0))
-		time.sleep(0.2)
+	if emptyCount > 3:
+		print("pause Music")
+		stopR = requests.get("http://192.168.2.149:5005/pauseAll")
+
+	sleep(1)
+	if (text == None):
+		emptyCount += 1
 	else:
-		print("Invalid URI")
-		pixels.fill((200, 0, 0))
-		time.sleep(0.2)
-		pixels.fill((0, 0, 0))
-		time.sleep(0.2)
+		emptyCount = 0
+		if (text.find("spotify") != -1 and previousRead != text):
+			getRequest = "http://192.168.2.149:5005/Foyer/spotify/now/" + text.strip()
+			r = requests.get(getRequest)
+			previousRead = text
+			pixels.fill((0, 200, 0))
+			time.sleep(0.2)
+			pixels.fill((0, 0, 0))
+			time.sleep(0.2)
+		else:
+			print("Invalid URI")
+			pixels.fill((200, 0, 0))
+			time.sleep(0.2)
+			pixels.fill((0, 0, 0))
+			time.sleep(0.2)
