fde6cf7688e04bb9b88efc03e400e1d7eacf5003
scheerp <patrick-scheer@web.de>
2022-05-26 19:53:45 +0200

    sdfsd

--- testReading.py
+++ testReading.py
@@ -33,6 +33,7 @@ try:
 	while True:
 		print("Hold a tag near the reader")
 		id, text = reader.read()
+		print(reader.read())
 		print("ID: %s\nText: %s" % (id,text))
 		sleep(5)
 except KeyboardInterrupt:
