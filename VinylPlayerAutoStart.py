3ee4530feb92ef1e3d4b51846710d58a560932f6
scheerp <patrick-scheer@web.de>
2022-05-26 20:21:50 +0200

    asdasd

--- VinylPlayerAutoStart.py
+++ VinylPlayerAutoStart.py
@@ -13,35 +13,39 @@ reader = SimpleMFRC522()
 emptyCount = 0
 previousRead = "nix"
 
-while True:
-	GPIO.cleanup()
-	print("EPMTYCOUNT: ", emptyCount)
-	print("Ready for some tunes!")
-	pixels.fill((40,0,80))
-	id, text = reader.read_no_block()
+try:
+	while True:
+		print("EPMTYCOUNT: ", emptyCount)
+		print("Ready for some tunes!")
+		pixels.fill((40,0,80))
+		id, text = reader.read_no_block()
 
-	if emptyCount > 3:
-		print("pause Music")
-		stopR = requests.get("http://192.168.2.149:5005/pauseAll")
+		if emptyCount > 3:
+			print("pause Music")
+			stopR = requests.get("http://192.168.2.149:5005/pauseAll")
 
-	time.sleep(1)
-	if (text == None):
-		emptyCount += 1
-	else:
-		emptyCount = 0
-		if (previousRead != text):
-			getRequest = "http://192.168.2.149:5005/Foyer/spotify/now/" + text.strip()
-			r = requests.get(getRequest)
-			previousRead = text
-			pixels.fill((0, 200, 0))
-			time.sleep(0.2)
-			pixels.fill((0, 0, 0))
-			time.sleep(0.2)
-		elif (text.find("spotify") != -1):
-			print("Invalid URI", text)
-			pixels.fill((200, 0, 0))
-			time.sleep(0.2)
-			pixels.fill((0, 0, 0))
-			time.sleep(0.2)
+		time.sleep(1)
+		if (text == None):
+			emptyCount += 1
 		else:
-			print("weiter wie gehabt")
+			emptyCount = 0
+			if (previousRead != text):
+				getRequest = "http://192.168.2.149:5005/Foyer/spotify/now/" + text.strip()
+				r = requests.get(getRequest)
+				previousRead = text
+				pixels.fill((0, 200, 0))
+				time.sleep(0.2)
+				pixels.fill((0, 0, 0))
+				time.sleep(0.2)
+			elif (text.strip().find("spotify") != -1):
+				print("weiter wie gehabt")
+			else:
+				print("Invalid URI", text)
+				pixels.fill((200, 0, 0))
+				time.sleep(0.2)
+				pixels.fill((0, 0, 0))
+				time.sleep(0.2)
+
+except KeyboardInterrupt:
+	GPIO.cleanup()
+	raise
