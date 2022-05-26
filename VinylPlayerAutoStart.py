9dada95abb368974a7a6b479d41863ddaae5bb40
scheerp <patrick-scheer@web.de>
2022-05-26 21:13:57 +0200

    v1.0

--- VinylPlayerAutoStart.py
+++ VinylPlayerAutoStart.py
@@ -12,19 +12,19 @@ reader = SimpleMFRC522()
 
 emptyCount = 0
 previousRead = "nix"
+pixels.fill((250, 200, 180))
 
 try:
 	while True:
-		print("EPMTYCOUNT: ", emptyCount)
-		print("Ready for some tunes!")
-		pixels.fill((40,0,80))
+		#print("Ready for some tunes!")
 		id, text = reader.read_no_block()
 
-		if emptyCount > 3:
-			print("pause Music")
+		if emptyCount > 4:
+			#print("pause Music")
 			stopR = requests.get("http://192.168.2.149:5005/pauseAll")
 			emptyCount = 0
 			previousRead = "nix"
+			pixels.fill((250, 200, 180))
 
 		time.sleep(1)
 		if (text == None):
@@ -32,21 +32,30 @@ try:
 		else:
 			emptyCount = 0
 			if (previousRead != text and text.strip().find("spotify") != -1):
+				print("play Music")
 				getRequest = "http://192.168.2.149:5005/Foyer/spotify/now/" + text.strip()
 				r = requests.get(getRequest)
 				previousRead = text
 				pixels.fill((0, 200, 0))
-				time.sleep(0.2)
-				pixels.fill((0, 0, 0))
-				time.sleep(0.2)
+				time.sleep(0.5)
+				pixels.fill((0, 200, 180))
 			elif (text.strip().find("spotify") != -1 and text != None):
-				print("weiter wie gehabt")
+				#print("weiter wie gehabt")
+				#pixels.fill((0, 200, 180))
 			else:
 				print("Invalid URI", text)
 				pixels.fill((200, 0, 0))
 				time.sleep(0.2)
 				pixels.fill((0, 0, 0))
+				time.sleep(0.1)
+				pixels.fill((200, 0, 0))
 				time.sleep(0.2)
+				pixels.fill((0, 0, 0))
+				time.sleep(0.1)
+				pixels.fill((200, 0, 0))
+				time.sleep(0.2)
+				pixels.fill((0, 0, 0))
+				time.sleep(0.1)
 
 except KeyboardInterrupt:
 	GPIO.cleanup()
