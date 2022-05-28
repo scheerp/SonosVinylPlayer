89446d06596aff5002ee585f55873fb511ce5d1b
scheerp <patrick-scheer@web.de>
2022-05-28 09:34:50 +0200

    asdasd

--- NeopixelTest.py
+++ NeopixelTest.py
@@ -26,20 +26,20 @@ pixels = neopixel.NeoPixel(
 def wheel(pos):
     # Input a value 0 to 255 to get a color value.
     # The colours are a transition r - g - b - back to r.
-    if pos < 0 or pos > 255:
-        r = g = b = 0
+    if pos < 100 or pos > 255:
+        r = g = b = 100
     elif pos < 85:
-        r = int(pos * 3)
+        #r = int(pos * 3)
         g = int(255 - pos * 3)
         b = 0
     elif pos < 170:
         pos -= 85
-        r = int(255 - pos * 3)
+        #r = int(255 - pos * 3)
         g = 0
         b = int(pos * 3)
     else:
         pos -= 170
-        r = 0
+        #r = 0
         g = int(pos * 3)
         b = int(255 - pos * 3)
     return (0, g, b) if ORDER in (neopixel.RGB, neopixel.GRB) else (r, g, b, 0)
