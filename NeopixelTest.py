95f35eb2497ea7420c78b9d025864279b7050914
scheerp <patrick-scheer@web.de>
2022-05-28 09:40:09 +0200

    asdasdsad

--- NeopixelTest.py
+++ NeopixelTest.py
@@ -30,8 +30,7 @@ def wheel(pos):
         r = g = b = 0
     elif pos < 85:
         #r = int(pos * 3)
-        g = 0
-        #g = int(255 - pos * 3)
+        g = int(255 + pos * 3)
         b = 0
     elif pos < 170:
         pos -= 85
@@ -42,8 +41,7 @@ def wheel(pos):
         pos -= 170
         #r = 0
         g = int(pos * 3)
-        b = 0
-        #b = int(255 - pos * 3)
+        b = int(255 + pos * 3)
     return (0, g, b) if ORDER in (neopixel.RGB, neopixel.GRB) else (r, g, b, 0)
 
 
