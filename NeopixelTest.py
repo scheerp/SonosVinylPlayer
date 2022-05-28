0d5c0f8265d1f9e55c654d4b01bbb1665defc2f5
scheerp <patrick-scheer@web.de>
2022-05-28 23:07:38 +0200

    dfdf

--- NeopixelTest.py
+++ NeopixelTest.py
@@ -29,20 +29,20 @@ def wheel(pos):
     if pos < 0 or pos > 255:
         r = g = b = 0
     elif pos < 85:
-        #r = int(pos * 3)
+        r = int(pos * 3)
         g = int(255 - pos * 3)
         b = 0
     elif pos < 170:
         pos -= 85
-        #r = int(255 - pos * 3)
+        r = int(255 - pos * 3)
         g = 0
         b = int(pos * 3)
     else:
         pos -= 170
-        #r = 0
+        r = 0
         g = int(pos * 3)
         b = int(255 - pos * 3)
-    return (0, g, b) if ORDER in (neopixel.RGB, neopixel.GRB) else (r, g, b, 0)
+    return (r, g, b) if ORDER in (neopixel.RGB, neopixel.GRB) else (r, g, b, 0)
 
 
 def rainbow_cycle(wait):
