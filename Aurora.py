81350c6ffd4d0d8099a1c0ccec2db5cf602c009e
scheerp <patrick-scheer@web.de>
2022-05-29 12:11:00 +0200

    asdas

--- Aurora.py
+++ Aurora.py
@@ -15,14 +15,38 @@ class Pixel:
     self.g = g
     self.b = b
 
+  g_factor = -3
+  b_factor = 3
+
   def colorPixel(self):
     time.sleep(0.03)
+    
+    if self.g < 0:
+      g_factor = 3
+    if self.g > 255:
+      g_factor = -3
+    
+    if self.b < 0:
+      b_factor = 3
+    if self.b > 255:
+      b_factor = -3
+
+    self.g += g_factor
+    self.b += b_factor
 
     pixels[self.index] = (0, self.g, self.b)
 
 p0 = Pixel(0, random.randint(100, 255), random.randint(100, 255))
 p1 = Pixel(1, random.randint(100, 255), random.randint(100, 255))
+p2 = Pixel(0, random.randint(100, 255), random.randint(100, 255))
+p3 = Pixel(1, random.randint(100, 255), random.randint(100, 255))
+p4 = Pixel(0, random.randint(100, 255), random.randint(100, 255))
+p5 = Pixel(1, random.randint(100, 255), random.randint(100, 255))
 
 while True:
   p0.colorPixel()
   p1.colorPixel()
+  p2.colorPixel()
+  p3.colorPixel()
+  p4.colorPixel()
+  p5.colorPixel()
