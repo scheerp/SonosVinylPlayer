37b72d5c35679abd3784c68f2ae3df213a640023
scheerp <patrick-scheer@web.de>
2022-05-29 12:42:55 +0200

    dsadasd

--- Aurora.py
+++ Aurora.py
@@ -15,26 +15,23 @@ class Pixel:
     self.g = g
     self.b = b
     self.g_factor =-3
+    self.b_factor =3
 
   def colorPixel(self):
-    #b_factor = 3
     
     if self.g < 3 or self.g > 252:
       self.g_factor = self.g_factor * -1
     
-    #if self.b < 3:
-      #b_factor = 3
-    #if self.b > 252:
-      #b_factor = -3
+    if self.b < 3 or self.b > 252:
+      self.b_factor = self.b_factor * -1
 
     self.g += self.g_factor
-    #self.b += b_factor
-    print('G:', self.g, 'FACTOR:', self.g_factor)
-    pixels[self.index] = (0, self.g, 0)
-    time.sleep(0.1)
+    self.b += self.b_factor
+    pixels[self.index] = (0, self.g, self.b)
+    time.sleep(0.05)
 
 p0 = Pixel(0, random.randint(100, 255), random.randint(100, 255))
-# p1 = Pixel(1, random.randint(100, 255), random.randint(100, 255))
+p1 = Pixel(1, random.randint(100, 255), random.randint(100, 255))
 # p2 = Pixel(2, random.randint(100, 255), random.randint(100, 255))
 # p3 = Pixel(3, random.randint(100, 255), random.randint(100, 255))
 # p4 = Pixel(4, random.randint(100, 255), random.randint(100, 255))
@@ -42,7 +39,7 @@ p0 = Pixel(0, random.randint(100, 255), random.randint(100, 255))
 
 while True:
   p0.colorPixel()
-  # p1.colorPixel()
+  p1.colorPixel()
   # p2.colorPixel()
   # p3.colorPixel()
   # p4.colorPixel()
