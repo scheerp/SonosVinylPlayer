eaebbf30e1ff7928c52dc2606eebc15f9f452a9c
scheerp <patrick-scheer@web.de>
2022-05-29 12:45:48 +0200

    asd

--- Aurora.py
+++ Aurora.py
@@ -19,28 +19,28 @@ class Pixel:
 
   def colorPixel(self):
     
-    if self.g < 3 or self.g > 252:
+    if self.g < 100 or self.g > 249:
       self.g_factor = self.g_factor * -1
     
-    if self.b < 3 or self.b > 252:
+    if self.b < 100 or self.b > 249:
       self.b_factor = self.b_factor * -1
 
-    self.g += self.g_factor
+    self.g += self.g_factor * random.randint(1, 5)
     self.b += self.b_factor
     pixels[self.index] = (0, self.g, self.b)
     time.sleep(0.05)
 
-p0 = Pixel(0, random.randint(100, 255), random.randint(100, 255))
-p1 = Pixel(1, random.randint(100, 255), random.randint(100, 255))
-# p2 = Pixel(2, random.randint(100, 255), random.randint(100, 255))
-# p3 = Pixel(3, random.randint(100, 255), random.randint(100, 255))
-# p4 = Pixel(4, random.randint(100, 255), random.randint(100, 255))
-# p5 = Pixel(5, random.randint(100, 255), random.randint(100, 255))
+p0 = Pixel(0, random.randint(100, 249), random.randint(100, 249))
+p1 = Pixel(1, random.randint(100, 249), random.randint(100, 249))
+p2 = Pixel(2, random.randint(100, 249), random.randint(100, 249))
+p3 = Pixel(3, random.randint(100, 249), random.randint(100, 249))
+p4 = Pixel(4, random.randint(100, 249), random.randint(100, 249))
+p5 = Pixel(5, random.randint(100, 249), random.randint(100, 249))
 
 while True:
   p0.colorPixel()
   p1.colorPixel()
-  # p2.colorPixel()
-  # p3.colorPixel()
-  # p4.colorPixel()
-  # p5.colorPixel()
+  p2.colorPixel()
+  p3.colorPixel()
+  p4.colorPixel()
+  p5.colorPixel()
