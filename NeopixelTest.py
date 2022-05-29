46f2b2555eca7865ceec468c1fcc869590fb17df
scheerp <patrick-scheer@web.de>
2022-05-29 09:39:20 +0200

    dasdas

--- NeopixelTest.py
+++ NeopixelTest.py
@@ -23,46 +23,23 @@ pixels = neopixel.NeoPixel(
     pixel_pin, num_pixels, brightness=0.4, auto_write=False, pixel_order=ORDER
 )
 
-g = random.randint(100, 255)
-b = random.randint(90, 255)
-
-def wheel2():
-    global g
-    global b
-    direction_g = random.choice([-3, 3])
-    direction_b = random.choice([-3, 3])
-    g += direction_g
-    b += direction_b
-
 def wheel(pos):
     # Input a value 0 to 255 to get a color value.
     # The colours are a transition r - g - b - back to r.
     if pos < 0 or pos > 255:
-        r = g = b = 0
+        g = b = 0
     elif pos < 85:
-        r = int(pos * 3)
         g = int(255 - pos * 3)
         b = 0
     elif pos < 170:
         pos -= 85
-        r = int(255 - pos * 3)
         g = 0
         b = int(pos * 3)
     else:
         pos -= 170
-        r = 0
         g = int(pos * 3)
         b = int(255 - pos * 3)
-    return (0, g, b) if ORDER in (neopixel.RGB, neopixel.GRB) else (r, g, b, 0)
-
-
-def aurora_cycle(wait):
-    for j in range(255):
-        for i in range(num_pixels):
-            pixel_index = (i * 256 // num_pixels) + j
-            pixels[i] = wheel2()
-        pixels.show()
-        time.sleep(wait)
+    return (0, g, b) if ORDER in (neopixel.RGB, neopixel.GRB) else (0, g, b, 0)
 
 def rainbow_cycle(wait):
     for j in range(255):
@@ -74,4 +51,4 @@ def rainbow_cycle(wait):
 
 
 while True:
-    aurora_cycle(0.03)  # rainbow cycle with 1ms delay per step
+    rainbow_cycle(0.03)  # rainbow cycle with 1ms delay per step
