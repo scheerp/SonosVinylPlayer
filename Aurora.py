748bff251b3e15c70d3738b63cc910679966f5b0
scheerp <patrick-scheer@web.de>
2022-05-29 11:27:21 +0200

    #xy#yx

--- Aurora.py
+++ Aurora.py
@@ -7,22 +7,7 @@ import board
 import neopixel
 import random
 
-
-# Choose an open pin connected to the Data In of the NeoPixel strip, i.e. board.D18
-# NeoPixels must be connected to D10, D12, D18 or D21 to work.
-pixel_pin = board.D18
-
-# The number of NeoPixels
-num_pixels = 6
-
-# The order of the pixel colors - RGB or GRB. Some NeoPixels have red and green reversed!
-# For RGBW NeoPixels, simply change the ORDER to RGBW or GRBW.
-ORDER = neopixel.GRB
-
-pixels = neopixel.NeoPixel(
-    pixel_pin, num_pixels, brightness=0.4, auto_write=False, pixel_order=ORDER
-)
-pixels[0] = (100, 150, 200)
+pixels = neopixel.NeoPixel(board.D18, 6)
 
 class Pixel:
   def __init__(self, index, g, b):
@@ -31,7 +16,6 @@ class Pixel:
     self.b = b
 
   def colorPixel(self):
-    print(self.index, self.g, self.b)
     pixels[self.index] = (0, self.g, self.b)
 
 
