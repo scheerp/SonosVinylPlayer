a320c655884e3fe3890d0d48c5d264b9db578de7
scheerp <patrick-scheer@web.de>
2022-05-29 09:22:28 +0200

    asdasd

--- NeopixelTest.py
+++ NeopixelTest.py
@@ -23,8 +23,8 @@ pixels = neopixel.NeoPixel(
     pixel_pin, num_pixels, brightness=0.4, auto_write=False, pixel_order=ORDER
 )
 
-global g = random.randint(100, 255)
-global b = random.randint(90, 255)
+g = random.randint(100, 255)
+b = random.randint(90, 255)
 
 def wheel2():
     g = g
