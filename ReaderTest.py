654a829fe10eb9f09c0ac547104ee7a747735b6d
scheerp <patrick-scheer@web.de>
2022-06-03 15:52:10 +0200

    asdasd

--- ReaderTest.py
+++ ReaderTest.py
@@ -1,3 +1,12 @@
-import nfc
-clf = nfc.ContactlessFrontend('usb')
-print(clf)
+from nfc import ContactlessFrontend
+import time
+
+def connected(tag):
+    ident = ''.join('{:02x}'.format(ord(c)) for c in tag.identifier)
+    print(ident)
+    return False
+
+clf = ContactlessFrontend('usb')
+while True:
+    clf.connect(rdwr={'on-connect': connected})
+    time.sleep(1)
