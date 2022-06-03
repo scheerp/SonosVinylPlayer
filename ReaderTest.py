f51aac947184da80184d2690432673f2665af4cc
scheerp <patrick-scheer@web.de>
2022-06-03 16:21:12 +0200

    asdasd

--- ReaderTest.py
+++ ReaderTest.py
@@ -1,16 +1,33 @@
-#!/usr/bin/env python
-# -*- coding: latin-1 -*-
+from smartcard.CardRequest import CardRequest
+from smartcard.Exceptions import CardRequestTimeoutException
+from smartcard.CardType import AnyCardType
+from smartcard import util
 
-import os
-import sys
-sys.path.insert(1, os.path.split(sys.path[0])[0])
+WAIT_FOR_SECONDS = 5
 
-import nfc
-import nfc.clf
-import nfc.tag
+if __name__ == '__main__':
+    # respond to the insertion of any type of smart card
+    card_type = AnyCardType()
 
-def connected(tag): print(tag.ndef.message.pretty() if tag.ndef else "Sorry, no NDEF"); return False
-clf = nfc.ContactlessFrontend('usb')
-tag = clf.connect(rdwr={'on-connect': connected})
+    # create the request. Wait for up to x seconds for a card to be attached
+    request = CardRequest(timeout=WAIT_FOR_SECONDS, cardType=card_type)
 
-clf.close()
+    # listen for the card
+    service = None
+    try:
+        service = request.waitforcard()
+    except CardRequestTimeoutException:
+        print("ERROR: No card detected")
+        exit(-1)
+
+    # when a card is attached, open a connection
+    conn = service.connection
+    conn.connect()
+
+    # get and print the ATR and UID of the card
+    get_uid = util.toBytes("FF CA 00 00 00")
+    print("ATR = {}".format(util.toHexString(conn.getATR())))
+    data, sw1, sw2 = conn.transmit(get_uid)
+    uid = util.toHexString(data)
+    status = util.toHexString([sw1, sw2])
+    print("UID = {}\tstatus = {}".format(uid, status))
