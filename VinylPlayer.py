a75ed5fdf14c151038457c7796e799daa682d767
scheerp <patrick-scheer@web.de>
2022-06-11 11:45:07 +0200

    asdasdasd

--- VinylPlayer.py
+++ VinylPlayer.py
@@ -7,50 +7,49 @@ import usersettings #this is the file you might need to edit
 import sys
 import board
 import RPi.GPIO as GPIO
-import neopixel
+#import neopixel
 import random
 from threading import Thread
 import threading
 
-pixels = neopixel.NeoPixel(board.D18, 6)
+#pixels = neopixel.NeoPixel(board.D18, 6)
 run_aurora_animation = False
 
-class Pixel:
-	def __init__(self, index, g, b):
-		self.index = index
-		self.g = g
-		self.b = b
-		self.g_factor =random.randint(-5, 5)
-		self.b_factor =random.randint(-5, 5)
+#class Pixel:
+#	def __init__(self, index, g, b):
+#		self.index = index
+#		self.g = g
+#		self.b = b
+#		self.g_factor =random.randint(-5, 5)
+#		self.b_factor =random.randint(-5, 5)
+#
+#	def colorPixel(self):
+#		if self.b_factor == 0:
+#			self.b_factor +=1
+#
+#		if self.g_factor == 0:
+#			self.g_factor += 1
+#		
+#		if self.g < 30 or self.g > 240:
+#			self.g_factor = self.g_factor * -1
+#		
+#		if self.b < 50 or self.b > 240:
+#			self.b_factor = self.b_factor * -1
+#
+#		self.g += self.g_factor
+#		self.b += self.b_factor
+#		pixels[self.index] = (0, self.g, self.b)
 
-	def colorPixel(self):
-		if self.b_factor == 0:
-			self.b_factor +=1
-
-		if self.g_factor == 0:
-			self.g_factor += 1
-		
-		if self.g < 30 or self.g > 240:
-			self.g_factor = self.g_factor * -1
-		
-		if self.b < 50 or self.b > 240:
-			self.b_factor = self.b_factor * -1
-
-		self.g += self.g_factor
-		self.b += self.b_factor
-		pixels[self.index] = (0, self.g, self.b)
-
-p0 = Pixel(0, random.randint(100, 240), random.randint(100, 240))
-p1 = Pixel(1, random.randint(100, 240), random.randint(100, 240))
-p2 = Pixel(2, random.randint(100, 240), random.randint(100, 240))
-p3 = Pixel(3, random.randint(100, 240), random.randint(100, 240))
-p4 = Pixel(4, random.randint(100, 240), random.randint(100, 240))
-p5 = Pixel(5, random.randint(100, 240), random.randint(100, 240))
+#p0 = Pixel(0, random.randint(100, 240), random.randint(100, 240))
+#p1 = Pixel(1, random.randint(100, 240), random.randint(100, 240))
+#p2 = Pixel(2, random.randint(100, 240), random.randint(100, 240))
+#p3 = Pixel(3, random.randint(100, 240), random.randint(100, 240))
+#p4 = Pixel(4, random.randint(100, 240), random.randint(100, 240))
+#p5 = Pixel(5, random.randint(100, 240), random.randint(100, 240))
 
 def released(tag):
 	global run_aurora_animation
 	print("Pause Music")
-	print(usersettings.sonoshttpaddress + "/pauseAll")
 	run_aurora_animation = False
 	stop_r = requests.get(usersettings.sonoshttpaddress + "/pauseAll")
 
@@ -212,7 +211,7 @@ if r.status_code == 200:
 	print ("... and API responding")
 
 
-pixels.fill((255, 204, 25))
+#pixels.fill((255, 204, 25))
 print("")
 print("OK, all ready! Present an NFC tag.")
 print("")
@@ -222,16 +221,16 @@ def update_aurora():
 
 	while True:
 		if run_aurora_animation:
-			p0.colorPixel()
-			p1.colorPixel()
-			p2.colorPixel()
-			p3.colorPixel()
-			p4.colorPixel()
-			p5.colorPixel()
+			#p0.colorPixel()
+			#p1.colorPixel()
+			#p2.colorPixel()
+			#p3.colorPixel()
+			#p4.colorPixel()
+			#p5.colorPixel()
 			time.sleep(0.01)
 		
 		else:
-			pixels.fill((255, 204, 25))
+			#pixels.fill((255, 204, 25))
 
 thread_aurora = Thread(target=update_aurora)
 
@@ -245,7 +244,7 @@ while True:
 		time.sleep(0.1);
 	
 	except KeyboardInterrupt:
-		pixels.fill((0, 0, 0))
+		#pixels.fill((0, 0, 0))
 		GPIO.cleanup()
 		raise
 
