e0fd5087ab74691503cc49979f4aa1a0fafe3e5a
scheerp <patrick-scheer@web.de>
2022-10-21 16:40:55 +0200

    esdf

--- VinylPlayer.py
+++ VinylPlayer.py
@@ -16,7 +16,6 @@ import os
 pixels = neopixel.NeoPixel(board.D18, 6)
 run_aurora_animation = False
 start_shutdown_sequence = False
-last_nfc_read = ""
 
 class Pixel:
 	def __init__(self, index, g, b):
@@ -53,10 +52,8 @@ p5 = Pixel(5, random.randint(100, 240), random.randint(100, 240))
 def released(tag):
 	global run_aurora_animation
 	global start_shutdown_sequence
-	global last_nfc_read
 	print("Pause Music")
 	run_aurora_animation = False
-	last_nfc_read = ""
 	stop_r = requests.get(usersettings.sonoshttpaddress + "/pauseAll")
 
 # this function gets called when a NFC tag is detected
@@ -64,7 +61,6 @@ def touched(tag):
 	global start_shutdown_sequence
 	global run_aurora_animation
 	global sonosroom_local
-	global last_nfc_read
 	print('Say hello', tag)
 
 	if tag.ndef:
@@ -190,17 +186,16 @@ def touched(tag):
 				r = requests.get(usersettings.sonoshttpaddress + "/" + sonosroom_local + "/clearqueue")
 
 			#use the request function to get the URL built previously, triggering the sonos
-			if last_nfc_read != urltoget:
+			if run_aurora_animation != True:
 				print ("Fetching URL via HTTP: "+ urltoget)
 				r = requests.get(urltoget)
 
 				if r.status_code != 200:
 					print ("Error code returned from Sonos API")
-					run_aurora_animation = True
-					last_nfc_read = urltoget
 					return True
 				
 				print ("Sonos API reports " + r.json()['status'])
+				run_aurora_animation = True
 
 	else:
 		print("")
